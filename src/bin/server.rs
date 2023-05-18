1896d4ecb2d4b104804c95524d2d9bb1650d28bd
boris <boris.lok@cienet.com>
2023-05-18 14:23:50 +0800

    [Test] write some tcpstream example.

---
+++ src/bin/server.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::time::{self, Duration};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+    let port = 12345;
+    let listener = TcpListener::bind(&format!("127.0.0.1:{}", port)).await?; 
+
+    if let Ok(stream) = accept(&listener).await {
+        println!("We have stream now, so we can send back some data here.");
+
+        loop {
+            stream.readable().await?;
+
+            let mut buf = [0;4096];
+
+            match stream.try_read(&mut buf) {
+                Ok(0) => {
+                    println!("Read EOF from stream.");
+                    break;
+                },
+                Ok(n) => {
+                    println!("read {} bytes", n);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(_) => {
+                    println!("Got an error");
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn accept(listener: &TcpListener) -> anyhow::Result<TcpStream> {
+    let mut backoff = 1;
+
+    loop {
+        match listener.accept().await {
+            Ok((socket, _)) => return Ok(socket),
+            Err(err) => {
+                if backoff > 64 {
+                    return Err(err.into());
+                }
+            }
+        }
+
+        time::sleep(Duration::from_secs(backoff)).await; 
+
+        backoff *= 2;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
-fn main() {
+use anyhow::Ok;
+use tokio::{net::TcpStream, io::AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+    let mut socket = TcpStream::connect("127.0.0.1:12345").await?;
+
+    // Write some data to socket
+    socket.write_u8(1).await?;
+
+    // flush all data
+    socket.flush().await?;
+
+    Ok(())
 }
