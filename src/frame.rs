b7a96d9b2d246cdc150381d03088da00e515a086
boris <boris.lok.tw@outlook.com>
2023-05-20 09:37:58 +0800

    Impl  Frame

---
+++ src/frame.rs
@@ -0,0 +1,132 @@
+use std::io::Cursor;
+
+use bytes::{Bytes, Buf};
+use std::num::TryFromIntError;
+
+/// A frame in  the Redis protocol.
+#[derive(Debug, Clone)]
+pub enum Frame {
+    Simple(String),
+    Integer(u64),
+    Bulk(Bytes),
+    Array(Vec<Frame>),
+    Null,
+    Error(String),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    /// Not enough data is available to parse message
+    Incomplete,
+
+    /// Invalid message enocoding
+    Other(crate::Error),
+}
+
+impl From<String> for Error {
+    fn from(src: String) -> Self {
+        Error::Other(src.into())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(src: &str) -> Self {
+        src.to_string().into()
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(_src: TryFromIntError) -> Self {
+        "protocol error; invalid frame format".into()
+    }
+}
+
+impl Frame {
+    pub fn check(src: &mut Cursor<&[u8]>) -> Result<(), Error> {
+        match get_u8(src)? {
+           b'+' => {
+               get_line(src)?;
+               Ok(())
+           }
+           b'-' => {
+            get_line(src)?;
+            Ok(())
+           }
+           b':' => {
+               let _ = get_decimal(src)?;
+               Ok(())
+           }
+           b'$' => {
+            if b'-' == peek_u8(src)? {
+                skip(src, 4)
+            } else {
+                // Read the bulk string
+                let len: usize= get_decimal(src)?.try_into()?;
+
+                // skip that number of bytes + 2(\r\n)
+                skip(src, len + 2)
+            }
+           }
+           b'*' => {
+            let len = get_decimal(src)?;
+
+            for _ in 0..len {
+                Frame::check(src)?;
+            }
+
+            Ok(())
+           }
+           actual =>  Err(format!("protocol error; invalid frame type byte `{}`", actual).into()),
+        }
+    }
+}
+
+fn peek_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+
+    Ok(src.chunk()[0])
+}
+
+fn get_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+
+    Ok(src.get_u8())
+}
+
+fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
+    if src.remaining() < n {
+        return Err(Error::Incomplete);
+    }
+
+    src.advance(n);
+    Ok(())
+}
+
+fn get_decimal(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
+    use atoi::atoi; 
+
+    let line = get_line(src)?;
+
+    atoi::<u64>(line).ok_or_else(|| "protocol error; invalid frame format".into())
+}
+
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    // Scan the bytes directly
+    let start = src.position() as usize;
+    // Scan to the second to last byte 
+    let end = src.get_ref().len() - 1;
+
+    for i in start..end {
+        if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
+            src.set_position((i + 2) as u64);
+
+            return Ok(&src.get_ref()[start..i]);
+        }
+    }
+
+    Err(Error::Incomplete)
+}
